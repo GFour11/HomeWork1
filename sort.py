7b7f9a0a9b95e95bb9873541fdb048e7b01de618
ulche <ulchegor1@gmaol.com>
2023-02-27 21:40:55 +0300

    first commit

---
+++ sort.py
@@ -0,0 +1,23 @@
+import os
+import defs
+
+
+
+def start():
+    while True:
+        file = input('Введіть шлях до папки яку потрібно відсортувати: ')
+        if os.path.isdir(file):
+            return file
+
+
+path=start()                                                     # Цикл запитує шлях до папки, яку потрібно відсортувати
+# create_new_dirs= defs.create_dirs(path)                          #Створюємо нові папки, куди будемо сортувати файли
+# first_sort=defs.sort_files(defs.get_file_paths(path), path)      #Сортуємо файли в першочеговій папці
+# second_sort = defs.full_sort(path)                               #Сортуємо файли в усіх інших папках, незважаючи на глибину вкладення
+# archives_unpacker= defs.dearchivator(path)                       #Розпаковуємо архіви
+# clean=defs.folder_remover(path)                                  #Видаляємо порожні папки
+result_file=defs.annotation_file(path)                           #Вертаємо ТХТ файл зі змістом відсортованої папки
+
+
+
+print('Ваші файли відсортовано')
